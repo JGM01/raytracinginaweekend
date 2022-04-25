25b42d60c7980632e49fdc59737e3e9846ea9420
JGM01 <jacobgermanamccray@gmail.com>
2022-04-24 23:15:52 -0400

    first commit

---
+++ build/CMakeFiles/raytracinginaweekend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracinginaweekend.dir/main.cpp.o"
+  "raytracinginaweekend"
+  "raytracinginaweekend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracinginaweekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacobgm/projects/raytracinginaweekend
+# Build directory: /home/jacobgm/projects/raytracinginaweekend/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
